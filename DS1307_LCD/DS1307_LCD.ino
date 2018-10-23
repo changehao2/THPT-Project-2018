06a0871c0d79156003a85f3f48d3387dc93e9dee
Kim <changehao2@gmail.com>
2018-10-23 11:51:29 +0700

    Add files via upload

---
+++ DS1307_LCD/DS1307_LCD.ino
@@ -0,0 +1,111 @@
+#include <Wire.h> 
+#include <LiquidCrystal.h>
+
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+/* Địa chỉ của DS1307 */
+const byte DS1307 = 0x68;
+/* Số byte dữ liệu sẽ đọc từ DS1307 */
+const byte NumberOfFields = 7;
+ 
+/* khai báo các biến thời gian */
+int second, minute, hour, day, wday, month, year;
+ 
+void setup()
+{
+  Wire.begin();
+  /* cài đặt thời gian cho module */
+//  setTime(20, 41, 20, 4, 21, 10, 18); // 12:30:45 CN 08-02-2015
+  Serial.begin(9600);
+  lcd.begin(20, 4);
+  
+}
+ 
+void loop()
+{
+  LCDTime();
+  delay(1000);
+}
+void LCDTime(){
+  lcd.clear();
+  lcd.setCursor(3,0);
+  lcd.print("Hello everyone!");
+  readDS1307();
+  lcd.setCursor(0,1);
+  lcd.print(hour);
+  lcd.print(":");
+  lcd.print(minute);
+  lcd.print(":");
+  lcd.print(second);
+  lcd.print(" ");
+  lcd.print(day);
+  lcd.print("/");
+  lcd.print(month);
+  lcd.print("/");
+  lcd.print(year);
+  
+}
+void readDS1307()
+{
+        Wire.beginTransmission(DS1307);
+        Wire.write((byte)0x00);
+        Wire.endTransmission();
+        Wire.requestFrom(DS1307, NumberOfFields);
+        
+        second = bcd2dec(Wire.read() & 0x7f);
+        minute = bcd2dec(Wire.read() );
+        hour   = bcd2dec(Wire.read() & 0x3f); // chế độ 24h.
+        wday   = bcd2dec(Wire.read() );
+        day    = bcd2dec(Wire.read() );
+        month  = bcd2dec(Wire.read() );
+        year   = bcd2dec(Wire.read() );
+        year += 2000;    
+}
+/* Chuyển từ format BCD (Binary-Coded Decimal) sang Decimal */
+int bcd2dec(byte num)
+{
+        return ((num/16 * 10) + (num % 16));
+}
+/* Chuyển từ Decimal sang BCD */
+int dec2bcd(byte num)
+{
+        return ((num/10 * 16) + (num % 10));
+}
+ 
+void digitalClockDisplay(){
+    // digital clock display of the time
+    Serial.print(hour);
+    printDigits(minute);
+    printDigits(second);
+    Serial.print(" ");
+    Serial.print(day);
+    Serial.print(" ");
+    Serial.print(month);
+    Serial.print(" ");
+    Serial.print(year); 
+    Serial.println(); 
+}
+ 
+void printDigits(int digits){
+    // các thành phần thời gian được ngăn chách bằng dấu :
+    Serial.print(":");
+        
+    if(digits < 10)
+        Serial.print('0');
+    Serial.print(digits);
+}
+ 
+/* cài đặt thời gian cho DS1307 */
+void setTime(byte hr, byte min, byte sec, byte wd, byte d, byte mth, byte yr)
+{
+        Wire.beginTransmission(DS1307);
+        Wire.write(byte(0x00)); // đặt lại pointer
+        Wire.write(dec2bcd(sec));
+        Wire.write(dec2bcd(min));
+        Wire.write(dec2bcd(hr));
+        Wire.write(dec2bcd(wd)); // day of week: Sunday = 1, Saturday = 7
+        Wire.write(dec2bcd(d)); 
+        Wire.write(dec2bcd(mth));
+        Wire.write(dec2bcd(yr));
+        Wire.endTransmission();
+}
